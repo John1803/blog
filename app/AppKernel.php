0325562ba9bdacd6e285f99bc2d6682f00c04f5f
will-o-the-wisp <will-o-the-wisp@blesschoice.blesschoice>
2014-01-02 17:19:01 +0200

    created BlogBlogBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Blog\Bundle\BlogBundle\BlogBlogBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
